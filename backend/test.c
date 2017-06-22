4d945ea0bad828fb4ba75f12988aa0ac9e2bd48a
dracarys09 <dubey.abhijeet09@gmail.com>
2017-06-23 01:56:38 +0530

    Modify test.c to test new changes.

--- backend/test.c
+++ backend/test.c
@@ -6,9 +6,19 @@ main (int argc, char **argv)
   GCPObject *gcp = gcp_object_new ();
 
   const gchar *access_token = "";
-  const gchar *printers = gcp_object_get_printers (gcp, access_token);
+  GList *printers = gcp_object_get_printers (gcp, access_token);
 
-  g_print ("%s\n", printers);
+  while(printers != NULL)
+  {
+    g_print ("%s\n", (gchar *)printers->data);
+    printers = printers->next;
+  }
+
+  // const gchar *uid = "__google__docs";
+  // const gchar *printer_options = gcp_object_get_printer_options (gcp, uid, access_token);
+
+  // g_print ("%s\n", printers);
+  // g_print ("%s\n", printer_options);
 
   g_object_unref (gcp);
   return 0;
